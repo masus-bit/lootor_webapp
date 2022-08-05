a42b0c91b60804ca27075edfa725d35c6b08fac2
masus-bit <maxumxumxum@gmail.com>
2022-08-05 14:43:48 +0300

    external api added

--- src/modules/Secured.module.ts
+++ src/modules/Secured.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './submodules/User.module';
 import { LotModule } from './submodules/Lot.module';
 import { DraftLot } from '../entities/DraftLot';
+import { ExternalModule } from './submodules/External.module';
 
 @Module({
   controllers: [],
   providers: [],
-  imports: [UserModule, LotModule, DraftLot],
+  imports: [UserModule, LotModule, DraftLot, ExternalModule],
 })
 export class SecuredModule {}
