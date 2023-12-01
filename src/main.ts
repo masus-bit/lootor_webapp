a1ccb6d99f0be51f901744c87cfc3066ec2e4d28
masus-bit <maxumxumxum@gmail.com>
2023-12-01 21:00:40 +0300

    cicd fix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
   const options = {
     cors: true,
   };
+
+  console.log(
+    process.env.POSTGRES_HOST,
+    process.env.POSTGRES_PORT,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    process.env.POSTGRES_DB,
+  );
   const app = await NestFactory.create(AppModule, options);
   const config = new DocumentBuilder()
     .setTitle('auc documentation')
