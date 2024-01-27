6b8731acd365ec99e8a9f385173987b69382953b
masus-bit <maxumxumxum@gmail.com>
2024-01-27 13:35:00 +0300

    фичи от 27-01-2024

--- src/entities/Games.ts
+++ src/entities/Games.ts
@@ -1,6 +1,5 @@
 import {
   Column,
-  Entity,
   Index,
   JoinColumn,
   JoinTable,
@@ -13,7 +12,7 @@ import { Genres } from './Genres';
 import { Platforms } from './Platforms';
 import { Developers } from './Developers';
 
-@Entity()
+// @Entity()
 export class Games {
   @PrimaryColumn()
   id: number;
--- src/modules/submodules/Games.module.ts
+++ src/modules/submodules/Games.module.ts
@@ -1,26 +1,18 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { UserRepository } from '../../repositories/User.repository';
-import { User } from '../../entities/User';
-import { JwtRegisterModule } from './jwt.module';
-import { HttpModule } from '@nestjs/axios';
-import { GamesRepository } from '../../repositories/Games.repository';
-import { GamesService } from '../../services/Games.service';
-import { Games } from '../../entities/Games';
-import { GamesController } from '../../controllers/Games.controller';
+
+// import { GamesController } from '../../controllers/Games.controller';
 
 @Module({
-  controllers: [GamesController],
-  providers: [UserRepository, GamesRepository, GamesService],
-  imports: [
-    TypeOrmModule.forFeature([User, Games]),
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
-    JwtRegisterModule,
-    HttpModule,
-  ],
-  exports: [GamesService],
+  // controllers: [GamesController],
+  // providers: [UserRepository, GamesRepository, GamesService],
+  // imports: [
+  //   TypeOrmModule.forFeature([User, Games]),
+  //   ConfigModule.forRoot({
+  //     envFilePath: '.env',
+  //   }),
+  //   JwtRegisterModule,
+  //   HttpModule,
+  // ],
+  // exports: [GamesService],
 })
 export class GamesModule {}
