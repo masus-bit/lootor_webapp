d73d8cd9af2b10b079f03acde0936c282f4c2a0d
masus-bit <maxumxumxum@gmail.com>
2024-04-17 21:57:18 +0300

    fix subscribes && public endpoints

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 export const ormconfig = new DataSource({
   migrationsTableName: 'migrations',
   type: 'postgres',
-  host: 'db_auc',
+  host: 'host.docker.internal',
   port: 5432,
   username: 'auc',
   password: 'aucer',
