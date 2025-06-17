1b51af40ddc90c78f75e8c8f6822ff7174c6402c
masus-bit <maxumxumxum@gmail.com>
2025-06-17 22:18:51 +0300

    fix payment

--- internal/pkg/payment/payment.go
+++ internal/pkg/payment/payment.go
@@ -110,7 +110,6 @@ func (s *PayService) EndTransaction(data *Notification) {
 	//years := 0
 
 	subType := data.Object.Metadata.Type
-	fmt.Println(data.Object)
 	if subType == "donate" {
 		return
 	} else {
