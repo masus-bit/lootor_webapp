1d32b3738686e33a127b8eab1584e8e067d32b57
masus-bit <maxumxumxum@gmail.com>
2025-09-26 12:11:30 +0300

    миграция с фиксом удаленных евентов

--- internal/migrations/005_fix_deleted_events.go
+++ internal/migrations/005_fix_deleted_events.go
@@ -13,21 +13,17 @@ func FixDeletedFlagEvents(db *gorm.DB) error {
 	if err := db.Exec(`
         UPDATE loot_events.events 
         SET deleted = true 
-        WHERE 
-            (target_user_login IS NOT NULL AND NOT EXISTS (
-                SELECT 1 FROM loot.users WHERE login = target_user_login AND deleted_at IS NULL
-            ))
-            OR (target_collection_id IS NOT NULL AND NOT EXISTS (
-                SELECT 1 FROM loot.collections WHERE id = target_collection_id AND deleted_at IS NULL
+        WHERE (target_collection_id IS NOT NULL AND NOT EXISTS (
+                SELECT 1 FROM loot.collections WHERE id::uuid = target_collection_id AND deleted_at IS NULL
             ))
             OR (target_item_id IS NOT NULL AND NOT EXISTS (
-                SELECT 1 FROM loot.collection_items WHERE id = target_item_id AND deleted_at IS NULL
+                SELECT 1 FROM loot.collection_items WHERE id::uuid = target_item_id AND deleted_at IS NULL
             ))
             OR (target_wish_list_item_id IS NOT NULL AND NOT EXISTS (
-                SELECT 1 FROM loot.wish_list_items WHERE id = target_wish_list_item_id AND deleted_at IS NULL
+                SELECT 1 FROM loot.wish_list_items WHERE id::uuid = target_wish_list_item_id AND deleted_at IS NULL
             ))
             OR (target_post_id IS NOT NULL AND NOT EXISTS (
-                SELECT 1 FROM loot_posts.posts WHERE id = target_post_id AND deleted_at IS NULL
+                SELECT 1 FROM loot_posts.posts WHERE id::text = target_post_id AND deleted_at IS NULL
             ))
     `).Error; err != nil {
 		return fmt.Errorf("failed to mark orphaned events as deleted: %w", err)
