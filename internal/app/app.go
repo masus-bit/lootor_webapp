c5135109e87edc130e330d01f1d8188a8b610b17
masus-bit <maxumxumxum@gmail.com>
2025-11-28 12:11:10 +0300

    like photo added to notifications

--- internal/app/app.go
+++ internal/app/app.go
@@ -186,7 +186,7 @@ func NewEchoApp(cfg *config.Config) (*App, error) {
 	eventsService := services.NewEventsService(userRepo, eventsClient, colRepo, ciRepo, postsClient, wlRepo, tagsClient, photosClient)
 	postsService := services.NewPostsService(postsClient, userRepo, eventsService, notificationsService, tagsClient)
 	s3Service := s3.NewS3Service(redisClient)
-	photosService := services.NewPhotosService(photosClient, userRepo, tagsClient, colRepo, eventsService, s3Service)
+	photosService := services.NewPhotosService(photosClient, userRepo, tagsClient, colRepo, eventsService, s3Service, notificationsService)
 
 	mailService := mail.NewMailService(host, portInt, user, password, `"Lootor" <noreply@lootor.me>`)
 	userService := services.NewUserService(userRepo, jwtService, ciRepo, mailService, eventsService, notificationsService, colRepo, postsService, tagsClient)
