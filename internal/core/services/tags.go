035f3bfa1839cd25f19f4d792ea5831b271e09c0
masus-bit <maxumxumxum@gmail.com>
2025-11-30 20:20:32 +0300

    исправлениђ

--- internal/core/services/tags.go
+++ internal/core/services/tags.go
@@ -145,15 +145,18 @@ func (s *TagsService) FindAllEntitiesByTag(tagID, entityType, limit, offset, aut
 	}
 	const maxInt32 = 1<<31 - 1
 
+	var finalLimit int64
+
 	intLimit64, err := strconv.ParseInt(limit, 10, 64)
 	if err != nil {
-		return nil, err
+		finalLimit = 0
 	}
-	if intLimit64 > maxInt32 {
+	finalLimit = intLimit64
+	if finalLimit > maxInt32 {
 		return nil, fmt.Errorf("limit too large: %d", intLimit64)
 	}
 
-	intLimit := int32(intLimit64)
+	intLimit := int32(finalLimit)
 	intOffset, _ := strconv.Atoi(offset)
 	tag, err := s.tagsClient.FindAllEntitiesByTag(context.Background(), &microservices.GetEntitiesByTagRequest{TagId: tagID, EntityType: entityType, Limit: int64(intLimit), Offset: int64(intOffset)})
 	if err != nil {
@@ -348,15 +351,18 @@ func (s *TagsService) Subscribe(id, userLogin string) (*dto.CommonResponse, erro
 func (s *TagsService) SearchSmartTags(query string, limit string) (*models.SearchResponse, error) {
 	const maxInt32 = 1<<31 - 1
 
+	var finalLimit int64
+
 	intLimit64, err := strconv.ParseInt(limit, 10, 64)
 	if err != nil {
-		return nil, err
+		finalLimit = 0
 	}
-	if intLimit64 > maxInt32 {
+	finalLimit = intLimit64
+	if finalLimit > maxInt32 {
 		return nil, fmt.Errorf("limit too large: %d", intLimit64)
 	}
 
-	intLimit := int32(intLimit64)
+	intLimit := int32(finalLimit)
 	resp, err := s.tagsClient.SearchGameTitles(context.Background(), &microservices.SearchGameTitlesRequest{
 		Query: query,
 		Limit: intLimit,
@@ -430,15 +436,18 @@ func (s *TagsService) RecordUserChoice(req *models.UserChoiceRequest) (*dto.Comm
 func (s *TagsService) GetSuggestions(query, limit string) (*models.SearchSuggestionResponse, error) {
 	const maxInt32 = 1<<31 - 1
 
+	var finalLimit int64
+
 	intLimit64, err := strconv.ParseInt(limit, 10, 64)
 	if err != nil {
-		return nil, err
+		finalLimit = 0
 	}
-	if intLimit64 > maxInt32 {
+	finalLimit = intLimit64
+	if finalLimit > maxInt32 {
 		return nil, fmt.Errorf("limit too large: %d", intLimit64)
 	}
 
-	intLimit := int32(intLimit64)
+	intLimit := int32(finalLimit)
 	resp, err := s.tagsClient.GetSearchSuggestions(context.Background(), &microservices.GetSearchSuggestionsRequest{
 		Query: query,
 		Limit: intLimit,
